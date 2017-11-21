f857b2f12c6ef93d1b341766e354e594ae1b8230
Divam Bansal <bansal.divam@gmail.com>
2017-11-21 10:45:59 +0530

    Final Commit

---
+++ main.js
@@ -0,0 +1,45 @@
+$('.welcome-screen button').on('click', function()   //dispaly name function
+  {
+      var name = $('#name-input').val();
+        if (name.length > 2) {
+           var message = "Welcome, " + name;
+            $('.main .user-name').text(message);
+            $('.welcome-screen').addClass('hidden');
+            $('.main').removeClass('hidden');
+        } else {
+           $('#name-input').addClass('error');
+        }
+ });
+
+
+$('body').on('keypress',function(event)   //display name on keypress enter
+{
+    if (event.keyCode == 13) //13 is keycode for ENTER
+      {
+        var name = $('#name-input').val();
+          if (name.length > 2)
+          {
+              var message = "Welcome, " + name;
+              $('.main .user-name').text(message);
+              $('.welcome-screen').addClass('hidden');
+              $('.main').removeClass('hidden');
+          } 
+           else
+           {
+             $('#name-input').addClass('error');
+            }
+       }
+});
+
+$('.login button').on('click', function()   //dispaly name function
+  {
+      var name = $('#name-input').val();
+        if (name.length > 2) {
+           var message = "Welcome, " + name;
+            $('.main .user-name').text(message);
+            $('.welcome-screen').addClass('hidden');
+            $('.main').removeClass('hidden');
+        } else {
+           $('#name-input').addClass('error');
+        }
+ });
